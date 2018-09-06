dc09d5c3786204028440bb994fbaebea0c76d90a
Sam Silverstein <samsilverstein@sams-macbook-air.local>
2018-09-06 15:53:20 -0400

    Initial Commit

---
+++ YeahIDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YeahIDK
+//
+//  Created by Sam Silverstein on 9/6/18.
+//  Copyright © 2018 Sam Silverstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
